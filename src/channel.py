65056cc68abc8fc8c83ba58897d02553d46b915e
Denis <denis30510@gmail.com>
2023-05-24 17:40:55 +0300

    added new until

--- src/channel.py
+++ src/channel.py
@@ -1,15 +1,15 @@
+import json
 import os
 from googleapiclient.discovery import build
-
-# YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-api_key: str = os.environ.get('YT_API_KEY')
-
-# создать специальный объект для работы с API
-youtube = build('youtube', 'v3', developerKey = api_key)
 
 
 class Channel:
     """Класс для ютуб - канала"""
+    # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+    api_key: str = os.environ.get('YT_API_KEY')
+
+    # создать специальный объект для работы с API
+    youtube = build('youtube', 'v3', developerKey = api_key)
 
 
     def __init__(self, channel_id: str) -> None:
@@ -17,9 +17,50 @@ class Channel:
         Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
 
+        self.title = self.youtube.channels().list \
+            (id = self.channel_id, part = 'snippet,statistics').execute() \
+            ['items'][0]['snippet']['title']
+
+        self.description = self.youtube.channels().list \
+            (id = self.channel_id, part = 'snippet,statistics').execute() \
+            ['items'][0]['snippet']['description']
+
+        self.url = 'https://www.youtube.com/channel/' + self.channel_id
+
+        self.subscriber_count = self.youtube.channels().list \
+            (id = self.channel_id, part = 'snippet,statistics').execute() \
+            ['items'][0]['statistics']['subscriberCount']
+
+        self.video_count = self.youtube.channels().list \
+            (id = self.channel_id, part = 'snippet,statistics').execute() \
+            ['items'][0]['statistics']['videoCount']
+
+        self.view_count = self.youtube.channels().list \
+            (id = self.channel_id, part = 'snippet,statistics').execute() \
+            ['items'][0]['statistics']['viewCount']
+
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list\
-            (id = self.channel_id, part = 'snippet,statistics').execute()
+        channel = self.youtube.channels().list\
+        (id = self.channel_id, part = 'snippet,statistics').execute()
         print(channel)
+
+
+    def to_json(self, name: str) -> None:
+        """
+        Запись класса в файл
+        """
+        a = self.__dict__
+        file = open(name, 'w')
+        json.dump(a, file)
+        file.close()
+
+
+
+    @classmethod
+    def get_service(cls):
+        return cls.youtube
+
+
